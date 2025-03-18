d37c1c7fe1c97b3998d38c16aa695676d89d96e2
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-18 14:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function convertToRoman(num) {
 					num -=obj[i];
 				}
 			}
-	return romanl
+	return roman;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
